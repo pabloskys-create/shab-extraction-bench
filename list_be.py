934aee3842d0456edd7a68a3dca09c00dbdda822
unknown <pabloskys123@gmail.com>
2026-09-02 13:26:47 +0200

    chore: remove temporary helper script

--- list_be.py
+++
@@ -1,5 +0,0 @@
-import json
-d = json.load(open('data/sampling/manifest_sample.json', encoding='utf-8'))
-for r in d['records']:
-    if r['canton'] == 'BE':
-        print(r['doc_id'], r['title'][:70])
